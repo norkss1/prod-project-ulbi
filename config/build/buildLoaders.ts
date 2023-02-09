a429169bd51da4bd6f967882fdf023655ff2cbc3
Roman Kalychak <home96.09@gmail.com>
2023-02-09 04:14:07 +0200

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,28 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {locales: ["ru", "en"]}
+                    ]
+                ]
+            }
+        }
+    }
+
     const cssLoaders = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -30,7 +52,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
     return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoaders,
     ]
---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from "react";
+import {useTranslation} from "react-i18next";
+import {AppRouter} from "app/providers/router";
+import {classNames} from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+import './styles/index.scss'
+
+const App = () => {
+    const { theme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
