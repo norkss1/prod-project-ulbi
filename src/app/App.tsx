a96808ef2ec7f612888c26fe7ca005578a38b39b
Roman Kalychak <home96.09@gmail.com>
2023-03-06 18:48:55 +0200

    add json-server

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,19 @@
 import { Suspense } from 'react';
 import { AppRouter } from 'app/providers/router';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
-const App = () => {
-    const { theme } = useTheme();
-
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-                <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
-        </div>
-    );
-};
+const App = () => (
+    <div className={classNames('app', {}, [])}>
+        <Suspense fallback="">
+            <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </Suspense>
+    </div>
+);
 
 export default App;
