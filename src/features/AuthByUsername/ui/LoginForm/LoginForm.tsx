0a709e57583853006ad9097d708d7fff3444cbcf
Roman Kalychak <home96.09@gmail.com>
2023-03-08 17:39:53 +0200

    add new input, add login form ui

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -0,0 +1,32 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import { Input } from 'shared/ui/Input/Input';
+import cls from './LoginForm.module.scss';
+
+interface LoginFormProps {
+    className?: string;
+}
+
+export const LoginForm = ({ className }: LoginFormProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.LoginForm, {}, [className])}>
+            <Input
+                autofocus
+                className={cls.input}
+                type="text"
+                placeholder={t('enter_username')}
+            />
+            <Input
+                className={cls.input}
+                type="text"
+                placeholder={t('enter_password')}
+            />
+            <Button className={cls.loginBtn}>
+                {t('signin')}
+            </Button>
+        </div>
+    );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Modal } from 'shared/ui/Modal/Modal';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +13,12 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
+    }, []);
+
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
     }, []);
 
     return (
@@ -22,15 +26,15 @@ export const Navbar = ({ className }: NavbarProps) => {
             <Button
                 theme={ButtonTheme.CLEAR_INVERTED}
                 className={cls.links}
-                onClick={onToggleModal}
+                onClick={onShowModal}
             >
                 {t('signin')}
             </Button>
 
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                {/* eslint-disable-next-line */}
-                {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, quos, similique. Eos fugiat illum itaque iure nam nihil placeat vel!')}
-            </Modal>
+            <LoginModal
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
         </div>
     );
 };
