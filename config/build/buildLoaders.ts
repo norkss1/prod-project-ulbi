1edfcc5daa2b7dec79ced05e5b3950620f2b19e8
Roman Kalychak <home96.09@gmail.com>
2023-04-05 18:37:46 +0300

    add new theme

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,29 +1,17 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
+
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        { locales: ['ru', 'en'] },
-                    ],
-                ],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader();
 
     const cssLoaders = buildCssLoader(isDev);
 
