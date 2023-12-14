101e8c5c5a43b32349924a9b875ba0aa1106bc0c
Vadym Babii <vbabii@endpointsnews.com>
2023-12-14 18:05:40 +0200

    add create, edit page routes

---
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
@@ -0,0 +1,25 @@
+import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { Page } from 'widgets/Page/Page';
+import cls from './ArticleEditPage.module.scss';
+
+interface ArticleEditPageProps {
+    className?: string;
+}
+
+const ArticleEditPage = memo((props: ArticleEditPageProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const { id } = useParams<{id: string}>();
+    const isEdit = Boolean(id);
+
+    return (
+        <Page className={classNames(cls.ArticleEditPage, {}, [className])}>
+            {isEdit ? 'asd' : ''}
+        </Page>
+    );
+});
+
+export default ArticleEditPage;
