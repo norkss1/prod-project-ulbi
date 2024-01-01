b4089281d77a2e741bc54e62cbad72999a17aa52
Roman Kalychak <home96.09@gmail.com>
2024-01-01 16:51:08 +0200

    update main.yml for github actions

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -5,9 +5,7 @@ type Selector<T> = (state: StateSchema) => T;
 type Result<T> = [() => T, Selector<T>]
 
 export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => {
-        return useSelector(selector);
-    };
+    const useSelectorHook = () => useSelector(selector);
 
     return [useSelectorHook, selector];
 }
