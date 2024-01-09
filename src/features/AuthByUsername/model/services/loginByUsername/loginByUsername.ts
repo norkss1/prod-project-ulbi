c450950792c9ae2fd70b6982cc7160a5a2ab9e65
Roman Kalychak <home96.09@gmail.com>
2024-01-09 17:34:17 +0200

    init auth data from server

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User, userActions } from '@/entities/User';
-import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 import { ThunkConfig } from '@/app/providers/StoreProvider';
 
 interface LoginByUsernameProps {
@@ -12,8 +11,8 @@ export const loginByUsername = createAsyncThunk<
     User,
     LoginByUsernameProps,
     ThunkConfig<string>
->('login/loginByUsername', async (authData, thunkAPI) => {
-    const { extra, dispatch, rejectWithValue } = thunkAPI;
+>('login/loginByUsername', async (authData, thunkApi) => {
+    const { extra, dispatch, rejectWithValue } = thunkApi;
 
     try {
         const response = await extra.api.post<User>('/login', authData);
@@ -22,12 +21,7 @@ export const loginByUsername = createAsyncThunk<
             throw new Error();
         }
 
-        localStorage.setItem(
-            USER_LOCALSTORAGE_KEY,
-            JSON.stringify(response.data),
-        );
         dispatch(userActions.setAuthData(response.data));
-
         return response.data;
     } catch (e) {
         console.log(e);
